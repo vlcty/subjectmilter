778e8609336268fbf28b89de5268df2b4cafcdc4
Josef 'veloc1ty' Stautner <hello@veloc1ty.de>
2021-03-09 18:22:47 +0100

    Replacing newlines with spaces

--- subjectmilter.go
+++ subjectmilter.go
@@ -56,6 +56,7 @@ func (e *MyFilter) Header(name, value string, m *milter.Modifier) (milter.Respon
 		if decodeError != nil {
 			fmt.Printf("Passing because of decode error: %s\n", decodeError.Error())
 		} else {
+			decoded = strings.ReplaceAll(decoded, "\n", " ")
 
 			fmt.Printf("Subject to analyze: \"%s\"\n", decoded)
 
